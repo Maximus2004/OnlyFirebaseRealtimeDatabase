421a8b5f278e75d6290cf6358dbe8c6d3dc79245
Maximus2004 <45393927+Maximus2004@users.noreply.github.com>
2019-05-01 22:04:30 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/maxim/realtimedatabasefirebase/MainActivity.java
+++ app/src/main/java/com/example/maxim/realtimedatabasefirebase/MainActivity.java
@@ -18,29 +18,27 @@ import java.util.HashMap;
 public class MainActivity extends AppCompatActivity {
     EditText loginEdit = findViewById(R.id.loginEditText);
     EditText passwordEdit = findViewById(R.id.passwordEditText);
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         View.OnClickListener oclBtnReg = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String textLogin = loginEdit.getText().toString();
                 String textPassword = passwordEdit.getText().toString();
                 FirebaseDatabase database = FirebaseDatabase.getInstance();
-                DatabaseReference databaseReference = database.getReference().child("realtimedatabase-7ee55");
+                databaseReference = database.getReference().child("realtimedatabase-7ee55");
                 HashMap<String, String> user = new HashMap<>();
                 user.put("login", textLogin);
                 databaseReference.setValue(user);
             }
         };
+
         Button registration = findViewById(R.id.registrationButton);
         registration.setOnClickListener(oclBtnReg);
-    }
-}
-
 
         /*databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
@@ -51,9 +49,10 @@ public class MainActivity extends AppCompatActivity {
                     Log.d("Student Name "," : "+studentData.get("Name"));
                 }
             }
-
             @Override
             public void onCancelled(DatabaseError databaseError) {
                 Log.e("ERROR",databaseError.toString());
             }
         });*/
+    }
+}
